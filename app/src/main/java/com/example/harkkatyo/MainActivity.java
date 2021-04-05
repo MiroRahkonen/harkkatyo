b10a2caf9c8c1e2adbe2d779ec6c35a9f11ef66e
Miro Rahkonen <rahkonenmiro000@gmail.com>
2021-04-05 10:18:44 +0300

    Activity ikkunat tehty

--- app/src/main/java/com/example/harkkatyo/MainActivity.java
+++ app/src/main/java/com/example/harkkatyo/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.harkkatyo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int x = 100;
-        int y = 200;
-        int z = 100;
-        int a = 100;
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.content_main);
+
+
+
+    }
+
+    public void gotoHousing(View v){
+        Intent intent = new Intent(MainActivity.this,HousingActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoVehicle(View v){
+        Intent intent = new Intent(MainActivity.this,VehicleActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoConsumption(View v){
+        Intent intent = new Intent(MainActivity.this,ConsumptionActivity.class);
+        startActivity(intent);
+    }
+
+    public void createSummary(View v){
+        Intent intent = new Intent(MainActivity.this,SummaryActivity.class);
+        startActivity(intent);
     }
 }
