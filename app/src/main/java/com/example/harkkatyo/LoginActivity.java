0c39dd058a94f22e16d138b08a982c9e9ed8f8d5
Teemu <Teemu.eloranta@student.lut.fi>
2021-04-12 21:07:12 +0300

    rekisteröinti työn alla

--- app/src/main/java/com/example/harkkatyo/LoginActivity.java
+++ app/src/main/java/com/example/harkkatyo/LoginActivity.java
@@ -5,18 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
+    EditText editText_EmailAddress, editText_Password;
+    Button button_confirm, button_register;
+    TextView textView2, textView3, textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editText_EmailAddress = (EditText) findViewById(R.id.editText_EmailAddress2);
+        editText_Password = (EditText) findViewById(R.id.editText_Password);
+        button_confirm = (Button) findViewById(R.id.button_confirm);
+        button_register = (Button) findViewById(R.id.button_register);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+
+
+
+        //Confirm
+        button_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //DoStuff
+                System.out.println("Testi1");
+            }
+        });
+
+        //Register
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Testi2");
+                //DoStuff
+            }
+        });
     }
+
 
 
     public void gotoMain(View v){
         Intent intent = new Intent(LoginActivity.this,MainActivity.class);
         startActivity(intent);
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/harkkatyo/RegisterActivity.java
+++ app/src/main/java/com/example/harkkatyo/RegisterActivity.java
@@ -3,12 +3,54 @@ package com.example.harkkatyo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editText_EmailAddress2, editText_Password;
+    Button button_confirm, button_return;
+    TextView textView2, textView3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button_confirm = (Button) findViewById(R.id.button_confirm);
+        button_return = (Button) findViewById(R.id.button_return);
+        editText_EmailAddress2 = (EditText) findViewById(R.id.editText_EmailAddress2);
+        editText_Password = (EditText) findViewById(R.id.editText_Password);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+
+        button_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Return
+            }
+        });
+        button_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Check things
+            }
+        });
+
+
+
+
     }
+    //Check for email input
+    private Boolean checkEmail() {
+        String test = editText_EmailAddress2.getText().toString();
+
+        if (test.isEmpty()) {
+            editText_EmailAddress2.setError("");
+        }
+    }
+
 }
