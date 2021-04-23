6c5b3c09cd54e4069dc4ef90c03ba5a75e3c5554
leevi <leevi.pussinen@live.com>
2021-04-23 15:06:55 +0300

    Consumption ja Vehicle pitäisi toimia

---
+++ app/src/main/java/com/example/harkkatyo/Vehicle.java
@@ -0,0 +1,40 @@
+package com.example.harkkatyo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Vehicle {
+
+    private static Vehicle vehicle = new Vehicle();
+
+    private double vehicleResult;
+    private int distance;
+    private int passengers;
+    private int year;
+    private String fuel;
+    private String size;
+
+    private Vehicle(){}
+
+    public static Vehicle getInstance(){ return vehicle; }
+
+    public double getVehicleResult(int distance, int passengers, int year, String fuel, String size){
+        this.distance = distance;
+        this.passengers = passengers;
+        this.year = year;
+        this.fuel = fuel;
+        this.size = size;
+        try {
+            String urlString = "https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/TransportCalculator/CarEstimate?query.buildYear=" + year + "&query.driveDistance=" + distance + "&query.size="+ size +"&query.fuel=" + fuel + "&query.passengerCount="+ passengers;
+            System.out.println(urlString);
+            InputStream input = JsonApi.newInstance().getInput(urlString);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            vehicleResult = Double.parseDouble(reader.readLine());
+        }catch (NumberFormatException | NullPointerException | IOException e) {
+            e.printStackTrace();
+        }
+        return vehicleResult;
+    }
+}
