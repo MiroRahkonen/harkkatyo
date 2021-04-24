b77330263dd90a30357b13a0d02b1efb95068365
Miro Rahkonen <rahkonenmiro000@gmail.com>
2021-04-24 13:25:59 +0300

    Summarya

--- app/src/main/java/com/example/harkkatyo/SummaryActivity.java
+++ app/src/main/java/com/example/harkkatyo/SummaryActivity.java
@@ -7,14 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.text.DecimalFormat;
 
 public class SummaryActivity extends AppCompatActivity {
 
     Consumption consumptionData;
     Housing housingData;
     Vehicle vehicleData;
-    TextView textView_ConsumptionResult,textView_HousingResult,textView_VehicleResult;
+    TextView textView_ConsumptionResult,textView_HousingResult,textView_VehicleResult,textView_SummaryTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +26,7 @@ public class SummaryActivity extends AppCompatActivity {
         textView_ConsumptionResult = findViewById(R.id.textView_SummaryConsumption);
         textView_HousingResult = findViewById(R.id.textView_SummaryHousing);
         textView_VehicleResult = findViewById(R.id.textView_SummaryVehicle);
+        textView_SummaryTotal = findViewById(R.id.textView_SummaryTotal);
         setSummaries();
     }
 
@@ -36,14 +36,17 @@ public class SummaryActivity extends AppCompatActivity {
     }
 
     public void setSummaries(){
+        double doubleTotal = consumptionData.getConsumptionTotal()+housingData.getResult()+vehicleData.getResult();
         String consumptionTotal = String.format("%.2f",consumptionData.getConsumptionTotal());
         String housingTotal = String.format("%.2f",housingData.getResult());
         String vehicleTotal = String.format("%.2f",vehicleData.getResult());
+        String allTotal = String.format("%.2f",doubleTotal);
 
 
-        textView_ConsumptionResult.setText("Consumption CO2-emissions in total: " + consumptionTotal);
-        textView_HousingResult.setText("Housing CO2-emissions in total: " + housingTotal);
-        textView_VehicleResult.setText("Vehicle CO2-emission in total: " + vehicleTotal);
+        textView_ConsumptionResult.setText("Consumption emissions(CO2 kg/year):\nTotal: " + consumptionTotal);
+        textView_HousingResult.setText("Housing emissions(CO2 kg/year):\nTotal: " + housingTotal);
+        textView_VehicleResult.setText("Vehicle emission(CO2 kg/year):\nTotal: " + vehicleTotal);
+        textView_SummaryTotal.setText("Total emissions(CO2 kg/year):\nTotal: "+allTotal);
     }
 
     public void cancel(View v){
